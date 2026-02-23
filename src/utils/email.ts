def0e99b2d69a979774eb012910cce43b61dca12
Rofiq354 <rofik010206@gmail.com>
2026-02-23 10:24:52 +0700

    fix: email notification from booking user

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -3,7 +3,10 @@ import "dotenv/config";
 import { AppError } from "../errors/AppError";
 
 const transporter = nodemailer.createTransport({
-  service: process.env.MAIL_SERVICE,
+  // service: process.env.MAIL_SERVICE,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
